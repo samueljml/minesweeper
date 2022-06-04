63806650c4dd6d05565e341c89e59839c193dec8
Samuel Andrade <devsamuelandrade@gmail.com>
2022-06-04 07:28:44 -0300

    Create app with Getx Architect

---
+++ android/app/src/main/kotlin/br/com/samueljml/minesweeper/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.samueljml.minesweeper
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const GetMaterialApp(
+    title: "Minesweeper",
+    debugShowCheckedModeBanner: false,
+    // theme: themeData,
+    // initialRoute: ,
+    // getPages: [],
+    // initialBinding: ,
+  ));
+}
