25e35745154a2a678f4ed91562a94c3438313ef5
Samuel Andrade <devsamuelandrade@gmail.com>
2022-06-11 18:34:12 -0300

    Add language service

---
+++ lib/app/data/services/language.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LanguageService extends GetxService {
+  static LanguageService get to => Get.find();
+
+  final Rx<Locale> _locale = const Locale('pt', 'BR').obs;
+  get getLocale => _locale.value;
+
+  final RxString _selectedLanguage = "EN".obs;
+  String get getSelectedLanguage => _selectedLanguage.value;
+
+  toggleLanguage() {
+    if (getSelectedLanguage == "PT") {
+      _selectedLanguage.value = "EN";
+      Get.back();
+      Get.updateLocale(const Locale('en', ''));
+    } else {
+      _selectedLanguage.value = "PT";
+      Get.back();
+      Get.updateLocale(const Locale('pt', 'BR'));
+    }
+  }
+}
