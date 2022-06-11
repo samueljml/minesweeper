4e36da7d20fdf9bb2dcd2e1fd5b6a443e62459d2
Samuel Andrade <devsamuelandrade@gmail.com>
2022-06-11 18:34:28 -0300

    Add changing language option

--- lib/app/global/home/page.dart
+++ lib/app/global/home/page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get/get.dart';
 import 'package:minesweeper/app/core/values/colors.dart';
 import 'package:minesweeper/app/global/home/controller.dart';
+
+import '../../data/services/language.dart';
 
 class Home extends GetView<HomeController> {
   const Home({Key? key}) : super(key: key);
@@ -24,138 +25,178 @@ class Home extends GetView<HomeController> {
         height: double.infinity,
         decoration: const BoxDecoration(color: secondaryColor),
         padding: const EdgeInsets.all(20),
-        child: Center(
-          child: Wrap(
-            alignment: WrapAlignment.center,
-            runSpacing: 20,
-            children: [
-              FittedBox(
-                fit: BoxFit.fill,
-                child: Text(AppLocalizations.of(context)!.title,
-                    style: Theme.of(context).textTheme.headline1),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: Row(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(border: Border.all(width: 4)),
-                      padding: const EdgeInsets.all(fieldsPadding),
-                      width: widgetsHeight,
-                      height: widgetsHeight,
-                      child: Center(
-                        child: FittedBox(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    LanguageService.to.toggleLanguage();
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(border: Border.all(width: 4)),
+                    padding: const EdgeInsets.all(fieldsPadding),
+                    width: widgetsHeight,
+                    height: widgetsHeight,
+                    child: Obx(
+                      () => Center(
+                        child: Text(LanguageService.to.getSelectedLanguage,
+                            style: fieldsTextStyle!
+                                .merge(const TextStyle(fontSize: 11))),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      runSpacing: 20,
+                      children: [
+                        FittedBox(
                           fit: BoxFit.fill,
-                          child: Text("*",
-                              style: Theme.of(context).textTheme.headline2),
+                          child: Text('title'.tr,
+                              style: Theme.of(context).textTheme.headline1),
                         ),
-                      ),
-                    ),
-                    Flexible(
-                      child: Container(
-                        height: widgetsHeight,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: fieldsPadding),
-                        decoration: const BoxDecoration(
-                          border: Border(
-                              top: borderSide,
-                              right: borderSide,
-                              bottom: borderSide),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Row(
+                            children: [
+                              Container(
+                                decoration:
+                                    BoxDecoration(border: Border.all(width: 4)),
+                                padding: const EdgeInsets.all(fieldsPadding),
+                                width: widgetsHeight,
+                                height: widgetsHeight,
+                                child: Center(
+                                  child: FittedBox(
+                                    fit: BoxFit.fill,
+                                    child: Text("*",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline2),
+                                  ),
+                                ),
+                              ),
+                              Flexible(
+                                child: Container(
+                                  height: widgetsHeight,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: fieldsPadding),
+                                  decoration: const BoxDecoration(
+                                    border: Border(
+                                        top: borderSide,
+                                        right: borderSide,
+                                        bottom: borderSide),
+                                  ),
+                                  child: TextFormField(
+                                    controller: textBombs,
+                                    keyboardType: TextInputType.number,
+                                    inputFormatters: <TextInputFormatter>[
+                                      FilteringTextInputFormatter.digitsOnly,
+                                      LengthLimitingTextInputFormatter(6)
+                                    ],
+                                    style: fieldsTextStyle,
+                                    cursorColor: Colors.black,
+                                    decoration: const InputDecoration(
+                                        border: InputBorder.none),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                        child: TextFormField(
-                          controller: textBombs,
-                          keyboardType: TextInputType.number,
-                          inputFormatters: <TextInputFormatter>[
-                            FilteringTextInputFormatter.digitsOnly,
-                            LengthLimitingTextInputFormatter(6)
-                          ],
-                          style: fieldsTextStyle,
-                          cursorColor: Colors.black,
+                        SizedBox(
+                          width: double.infinity,
+                          child: Row(children: [
+                            Flexible(
+                              flex: 3,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: fieldsPadding),
+                                decoration:
+                                    BoxDecoration(border: Border.all(width: 4)),
+                                height: widgetsHeight,
+                                child: TextFormField(
+                                  controller: textWidth,
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: <TextInputFormatter>[
+                                    FilteringTextInputFormatter.digitsOnly,
+                                    LengthLimitingTextInputFormatter(3)
+                                  ],
+                                  style: fieldsTextStyle,
+                                  cursorColor: Colors.black,
+                                  decoration: const InputDecoration(
+                                      border: InputBorder.none),
+                                ),
+                              ),
+                            ),
+                            Flexible(
+                              flex: 1,
+                              child: Center(
+                                  child: Text(
+                                "x",
+                                style: fieldsTextStyle,
+                              )),
+                            ),
+                            Flexible(
+                              flex: 3,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: fieldsPadding),
+                                decoration:
+                                    BoxDecoration(border: Border.all(width: 4)),
+                                height: widgetsHeight,
+                                child: TextFormField(
+                                  controller: textHeight,
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: <TextInputFormatter>[
+                                    FilteringTextInputFormatter.digitsOnly,
+                                    LengthLimitingTextInputFormatter(3)
+                                  ],
+                                  style: fieldsTextStyle,
+                                  cursorColor: Colors.black,
+                                  decoration: const InputDecoration(
+                                      border: InputBorder.none),
+                                ),
+                              ),
+                            ),
+                          ]),
+                        ),
+                        Container(
+                          height: widgetsHeight,
                           decoration:
-                              const InputDecoration(border: InputBorder.none),
+                              BoxDecoration(border: Border.all(width: 4)),
+                          width: double.infinity,
+                          child: OutlinedButton(
+                            child: Text(
+                              'startGameButtonText'.tr.toUpperCase(),
+                              style: fieldsTextStyle,
+                            ),
+                            onPressed: () {},
+                            style: OutlinedButton.styleFrom(
+                              primary: Colors.black,
+                              onSurface: Colors.red,
+                              shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.zero)),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: Row(children: [
-                  Flexible(
-                    flex: 3,
-                    child: Container(
-                      padding:
-                          const EdgeInsets.symmetric(horizontal: fieldsPadding),
-                      decoration: BoxDecoration(border: Border.all(width: 4)),
-                      height: widgetsHeight,
-                      child: TextFormField(
-                        controller: textWidth,
-                        keyboardType: TextInputType.number,
-                        inputFormatters: <TextInputFormatter>[
-                          FilteringTextInputFormatter.digitsOnly,
-                          LengthLimitingTextInputFormatter(3)
-                        ],
-                        style: fieldsTextStyle,
-                        cursorColor: Colors.black,
-                        decoration:
-                            const InputDecoration(border: InputBorder.none),
-                      ),
+                      ],
                     ),
                   ),
-                  Flexible(
-                    flex: 1,
-                    child: Center(
-                        child: Text(
-                      "x",
-                      style: fieldsTextStyle,
-                    )),
-                  ),
-                  Flexible(
-                    flex: 3,
-                    child: Container(
-                      padding:
-                          const EdgeInsets.symmetric(horizontal: fieldsPadding),
-                      decoration: BoxDecoration(border: Border.all(width: 4)),
-                      height: widgetsHeight,
-                      child: TextFormField(
-                        controller: textHeight,
-                        keyboardType: TextInputType.number,
-                        inputFormatters: <TextInputFormatter>[
-                          FilteringTextInputFormatter.digitsOnly,
-                          LengthLimitingTextInputFormatter(3)
-                        ],
-                        style: fieldsTextStyle,
-                        cursorColor: Colors.black,
-                        decoration:
-                            const InputDecoration(border: InputBorder.none),
-                      ),
-                    ),
-                  ),
-                ]),
+                ],
               ),
-              Container(
-                height: widgetsHeight,
-                decoration: BoxDecoration(border: Border.all(width: 4)),
-                width: double.infinity,
-                child: OutlinedButton(
-                  child: Text(
-                    AppLocalizations.of(context)!
-                        .startGameButtonText
-                        .toUpperCase(),
-                    style: fieldsTextStyle,
-                  ),
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    primary: Colors.black,
-                    onSurface: Colors.red,
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.zero)),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get/get.dart';
+import 'package:minesweeper/app/data/services/language.dart';
 
 import 'app/core/theme/theme.dart';
+import 'app/core/utils/locale_string.dart';
 import 'app/routes/pages/pages.dart';
 
-void main() {
+void main() async {
   runApp(GetMaterialApp(
+    translations: LocaleString(),
+    initialBinding: BindingsBuilder(() {
+      Get.put(LanguageService());
+    }),
     title: "Minesweeper",
     debugShowCheckedModeBanner: false,
     theme: themeData,
+    locale: const Locale('en', 'US'),
     initialRoute: "/",
     getPages: Pages.routes,
-    supportedLocales: const [
-      Locale('en', ''),
-      Locale('pt', 'BR'),
-    ],
-    localizationsDelegates: AppLocalizations.localizationsDelegates,
-    defaultTransition: Transition.fadeIn,
   ));
 }
