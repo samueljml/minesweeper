c591470ec284f94e948803ab9075fa2033721f25
Samuel Andrade <devsamuelandrade@gmail.com>
2022-06-16 12:12:11 -0300

    Create widget to game modal

--- lib/app/modules/game/page.dart
+++ lib/app/modules/game/page.dart
@@ -3,97 +3,24 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
 import '../../global_widgets/mobile_screen.dart';
-import '../../global_widgets/modal.dart';
 import 'controller.dart';
+import 'widgets/back_home_modal.dart';
 
 class Game extends GetView<GameController> {
   const Game({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final TextStyle? fieldsTextStyle = Theme.of(context).textTheme.headline5;
-
     return MobileScreen(
         child: Column(
       children: [
         Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           IconButton(
             icon: const Icon(FontAwesomeIcons.arrowLeft),
-            onPressed: () => Modal.body(
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            "confirmation".tr,
-                            style: fieldsTextStyle,
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      Row(
-                        children: [
-                          Text(
-                            "backToHomeScreen".tr,
-                            maxLines: 2,
-                            softWrap: true,
-                            overflow: TextOverflow.ellipsis,
-                            style: Theme.of(context).textTheme.bodyText2,
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(border: Border.all(width: 3)),
-                        height: 40,
-                        width: 80,
-                        child: OutlinedButton(
-                          child: FittedBox(
-                            fit: BoxFit.fill,
-                            child: Text("no".tr,
-                                style: Theme.of(context).textTheme.headline6),
-                          ),
-                          onPressed: () => Get.back(),
-                          style: OutlinedButton.styleFrom(
-                            primary: Colors.black,
-                            shape: const RoundedRectangleBorder(
-                                borderRadius: BorderRadius.all(Radius.zero)),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(border: Border.all(width: 3)),
-                        height: 40,
-                        width: 80,
-                        child: OutlinedButton(
-                          child: FittedBox(
-                            fit: BoxFit.fill,
-                            child: Text("yes".tr,
-                                style: Theme.of(context).textTheme.headline6),
-                          ),
-                          onPressed: () => Get.toNamed("/home"),
-                          style: OutlinedButton.styleFrom(
-                            primary: Colors.black,
-                            shape: const RoundedRectangleBorder(
-                                borderRadius: BorderRadius.all(Radius.zero)),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
+            onPressed: () {
+              controller.togglePauseGame();
+              BackHomeModal.show(controller.togglePauseGame);
+            },
           ),
           Wrap(
             spacing: 10,
---
+++ lib/app/modules/game/widgets/back_home_modal.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../core/theme/text_styles.dart';
+import '../../../global_widgets/modal.dart';
+
+class BackHomeModal {
+  static show(void Function() onClose) => Modal.body(
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "confirmation".tr,
+                      style: TextStyles.defaultStyle,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      "backToHomeScreen".tr,
+                      maxLines: 2,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyles.bodyText,
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  decoration: BoxDecoration(border: Border.all(width: 3)),
+                  height: 40,
+                  width: 80,
+                  child: OutlinedButton(
+                    child: FittedBox(
+                      fit: BoxFit.fill,
+                      child: Text("no".tr, style: TextStyles.button),
+                    ),
+                    onPressed: () {
+                      onClose();
+                      Get.back();
+                    },
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.black,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.zero)),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  decoration: BoxDecoration(border: Border.all(width: 3)),
+                  height: 40,
+                  width: 80,
+                  child: OutlinedButton(
+                    child: FittedBox(
+                      fit: BoxFit.fill,
+                      child: Text("yes".tr, style: TextStyles.button),
+                    ),
+                    onPressed: () => Get.toNamed("/home"),
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.black,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.zero)),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+        onClose: onClose,
+      );
+}
